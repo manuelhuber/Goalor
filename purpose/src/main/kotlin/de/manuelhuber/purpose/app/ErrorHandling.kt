b69488ba3785ae47e6670c995ed307394688eb77
Grim <dev@manuel-huber.de>
2019-12-24 12:38:44 +0000

    fixed error responses

--- purpose/src/main/kotlin/de/manuelhuber/purpose/app/ErrorHandling.kt
+++ purpose/src/main/kotlin/de/manuelhuber/purpose/app/ErrorHandling.kt
@@ -2,6 +2,7 @@ package de.manuelhuber.purpose.app
 
 import de.manuelhuber.purpose.features.auth.models.NotAuthorized
 import de.manuelhuber.purpose.features.auth.models.WrongPassword
+import de.manuelhuber.purpose.lib.controller.ErrorResponse
 import de.manuelhuber.purpose.lib.engine.NotFound
 import io.javalin.Javalin
 import org.slf4j.Logger
@@ -9,14 +10,18 @@ import org.slf4j.Logger
 // TODO use ErrorResponse
 fun addErrorHandling(app: Javalin, logger: Logger) {
     app
-        .exception(Exception::class.java) { exception, _ ->
+        .exception(Exception::class.java) { exception, ctx ->
             logger.error("uncaught", exception)
+            ctx.status(500).json(ErrorResponse(exception.message ?: ""))
         }
         .exception(NotFound::class.java) { exception, ctx ->
             logger.info(exception.message)
             ctx.status(404)
-                .result(exception.message.orEmpty())
+                .json(ErrorResponse(exception.message.orEmpty()))
         }
-        .exception(WrongPassword::class.java) { _, ctx -> ctx.status(401).result("Wrong password") }
-        .exception(NotAuthorized::class.java) { exception, ctx -> ctx.status(401).result(exception.message) }
+        .exception(WrongPassword::class.java) { _, ctx -> ctx.status(401).json(ErrorResponse("Wrong password")) }
+        .exception(NotAuthorized::class.java) { exception, ctx ->
+            ctx.status(401)
+                .json(ErrorResponse(exception.message))
+        }
 }
